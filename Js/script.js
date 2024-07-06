b11bd441c8092fa8ffdba8d846e9194ebfdfbaa0
MittapellyManohar <mittapellymanohar1991@gmail.com>
2024-07-06 13:20:03 -0700

    Initial commit

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+.header .first img {
+	width: 130px;
+	margin: 0px 30px 0px 25px;
+}
+.header .first h2 {
+	font-size: 25px;
+	font-weight: bold;
+	color: #666;
+	border-left: solid 2px #000000;
+	padding: 0px 35px;
+}
